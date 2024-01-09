f2bfa2ada607ffc8cb3db4ee3a4744641a31e4c0
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-10 08:48:03 +0900

    Title: 정사각형 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240110/정사각형 별표 출력하기/print-stars-in-square.cpp
+++ 240110/정사각형 별표 출력하기/print-stars-in-square.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 int main() {
+    int n;
+    std::cout << n;
+    std::cin >> n;
 
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
             std::cout << "*";
         }
         std::cout << std::endl;
