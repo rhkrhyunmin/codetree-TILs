a5eb106002b35ba19ecd31eee0c9cf047d2e6619
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-11 08:13:23 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240111/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+void poly()
+{
+    int n = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "12345^&*()";
+    }
+    cout << endl;
+}
+
+int main() {
+    for (int i = 0; i < 5; i++)
+    {
+        poly();
+    }
+    return 0;
+}
