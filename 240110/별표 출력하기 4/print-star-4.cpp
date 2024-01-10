d2e158cd25f193576fe680c68e97f90259e941e8
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-10 09:24:39 +0900

    Title: 별표 출력하기 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/별표 출력하기 4/print-star-4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    for (int i = n; i > 0; --i) {
+        for (int j = 1; j <= i; ++j) {
+            std::cout << "* ";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 2; i <= n; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            std::cout << "* ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
