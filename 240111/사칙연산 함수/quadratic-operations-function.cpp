fe5784f219f8ffed2488e69472237e65a4509411
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-11 10:09:30 +0900

    Title: 사칙연산 함수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240111/사칙연산 함수/quadratic-operations-function.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+    int a, c;
+    char o;
+
+    std::cin >> a >> o >> c;
+
+    switch (o) {
+    case '+':
+        std::cout << a << " + " << c << " = " << a + c << std::endl;
+        break;
+    case '-':
+        std::cout << a << " - " << c << " = " << a - c << std::endl;
+        break;
+    case '*':
+        std::cout << a << " * " << c << " = " << a * c << std::endl;
+        break;
+    case '/':
+        if (c != 0) {
+            std::cout << a << " / " << c << " = " << static_cast<double>(a) / c << std::endl;
+        }
+        break;
+    }
+    return 0;
+}
