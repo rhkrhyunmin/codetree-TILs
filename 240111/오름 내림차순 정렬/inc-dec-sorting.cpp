b0a7be60dda15fea7e319a5e5f8a4ac9f24f7110
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-11 10:47:06 +0900

    Title: 오름 내림차순 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240111/오름 내림차순 정렬/inc-dec-sorting.cpp
+++ 240111/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -12,6 +12,11 @@ int main() {
     for (int i = 0; i < n; ++i) {
         std::cin >> arr[i];
     }
+    std::sort(arr.begin(), arr.end());
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
 
     std::sort(arr.rbegin(), arr.rend());
     for (int i = 0; i < n; ++i) {
