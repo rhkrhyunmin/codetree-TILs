bba41783665df9a925b9a03aab2673b296c41dea
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-12 08:16:54 +0900

    Title: 줄 세우기 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240112/줄 세우기 2/line-up-students-2.cpp
+++ 240112/줄 세우기 2/line-up-students-2.cpp
@@ -7,15 +7,12 @@ using namespace std;
 
 bool cmp(tuple<int, int, int> a, tuple<int, int, int> b)
 {
+
     if (get<0>(a) != get<0>(b)) {
         return get<0>(a) < get<0>(b);
     }
 
-    if (get<1>(a) != get<1>(b)) {
-        return get<1>(a) < get<1>(b);
-    }
-
-    return get<2>(a) < get<2>(b);
+    return get<1>(a) > get<1>(b);
 }
 
 int main()
