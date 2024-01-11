f60f3fbc15420c114ff2425aa0f4878fc374b652
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-11 10:30:03 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240111/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+// 재귀 함수로 삼각형 출력
+void printTriangle(int n) {
+    if (n == 0) {
+        return;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        std::cout << "* ";
+    }
+    std::cout << std::endl;
+
+    printTriangle(n - 1);
+}
+
+void printReverseTriangle(int n) {
+    if (n == 0) {
+        return;
+    }
+
+    printReverseTriangle(n - 1);
+
+    for (int i = 0; i < n; ++i) {
+        std::cout << "* ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    int N;
+
+    // 입력 받기
+    std::cout << "Enter an integer N: ";
+    std::cin >> N;
+
+    // 삼각형 출력
+    printTriangle(N);
+
+    // 역삼각형 출력
+    printReverseTriangle(N - 1);
+
+    return 0;
+}
