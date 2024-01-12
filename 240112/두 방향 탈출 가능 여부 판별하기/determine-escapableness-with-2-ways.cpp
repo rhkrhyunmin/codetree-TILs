f97a8ed8086baaa54bea32dc1364304b8cbd6c66
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-12 09:35:27 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240112/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 240112/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -7,30 +7,30 @@ using namespace std;
 int n, m;
 int vertex_cnt;
 
-int dx[] = { 0, 1 };
-int dy[] = { -1, 0 };
+int dx[] = {0, 1};
+int dy[] = {-1, 0};
 
 // index를 1번 부터 사용하기 위해 MAX_NUM+1만큼 할당합니다.
-int graph[MAX_NUM + 1][MAX_NUM + 1];
-bool visited[MAX_NUM + 1][MAX_NUM + 1];
+int graph[MAX_NUM][MAX_NUM];
+bool visited[MAX_NUM][MAX_NUM];
 
 void DFS(int x, int y) {
     // 해당 정점에서 이어져있는 모든 정점을 탐색해줍니다.
     for (int i = 0; i < 2; i++) {
-        int ny = y + dy[i];
         int nx = x + dx[i];
+        int ny = y + dy[i];
 
         // 범위를 벗어나는 경우에는 건너뜁니다.
-        if (ny < 0 || ny >= n || nx < 0 || nx >= m) {
+        if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
             continue;
         }
 
         // 아직 방문하지 않았고, 간선이 존재하는 경우에만 DFS를 호출합니다.
-        if (graph[ny][nx] == 1 && !visited[ny][nx]) {
-            visited[ny][nx] = true;
+        if (graph[nx][ny] == 1 && !visited[nx][ny]) {
+            visited[nx][ny] = true;
 
             // 현재 정점에서 이어진 정점에 대해서도 DFS를 호출합니다.
-            DFS(ny, nx);
+            DFS(nx, ny);
         }
     }
 }
@@ -51,7 +51,7 @@ int main() {
     visited[0][0] = true;
     DFS(0, 0);
 
-    cout << visited[m - 1][m - 1];
+    cout << visited[n - 1][m - 1];
 
     return 0;
 }
