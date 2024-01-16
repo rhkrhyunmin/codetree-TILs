e76003b1ddde5a2c403d2efc865aeca71650db11
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-16 19:26:03 +0900

    Title: 소수점 반올림하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240116/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath> // round 함수를 사용하기 위한 헤더 파일
+
+int main() {
+    // 사용자로부터 입력 받기
+    double a;
+    std::cin >> a;
+
+    // 입력받은 값 반올림
+    double rounded_a = round(a * 10) / 10;
+
+    // 결과 출력
+    std::cout << rounded_a << std::endl;
+
+    return 0;
+}
