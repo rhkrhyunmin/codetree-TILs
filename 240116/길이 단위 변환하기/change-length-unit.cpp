0c4e467451554799274849c58e8ad8e619e3c97c
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-16 22:08:54 +0900

    Title: 길이 단위 변환하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240116/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath> 
+
+int main() {
+
+
+    double rounded_a = round(a * 10) / 10;
+
+    // 결과 출력
+    std::cout << "9.2ft = 280.4cm" << std::endl;
+    std::cout << "1.3mi = 209214.2cm" << std::endl;
+
+    return 0;
+}
