75211ebe0b5a0f5224a3fc7d3e9b22abdf0b2a93
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-17 21:35:30 +0900

    Title: 평균점수 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240117/평균점수 구하기/print-average.cpp
+++ 240117/평균점수 구하기/print-average.cpp
@@ -6,7 +6,6 @@ int main() {
     double scores[8];
 
     // 8명의 학생들의 점수를 입력받음
-    std::cout << "8명의 학생들의 점수를 입력하세요 (100점 이하): ";
     for (int i = 0; i < 8; ++i) {
         std::cin >> scores[i];
     }
@@ -20,7 +19,7 @@ int main() {
 
     // 소수 첫째 자리까지 출력
     std::cout << std::fixed << std::setprecision(1);
-    std::cout << "평균 점수: " << average << std::endl;
+    std::cout << average << std::endl;
 
     return 0;
 }
