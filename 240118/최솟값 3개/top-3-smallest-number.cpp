71f1941414033a9f182b50ccfd1a06fe357aeaba
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-18 11:02:18 +0900

    Title: 최솟값 3개, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240118/최솟값 3개/top-3-smallest-number.cpp
+++ 240118/최솟값 3개/top-3-smallest-number.cpp
@@ -1,29 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
+
+using namespace std;
 
 int main() {
     int n;
-    std::cin >> n;
+    cin >> n;
 
-    std::vector<int> numbers;
-    
+    vector<int> numbers;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
     for (int i = 0; i < n; ++i) {
         int num;
-        std::cin >> num;
-
+        cin >> num;
         numbers.push_back(num);
 
-        if (numbers.size() >= 3) {
-            // 현재까지 주어진 숫자 중 가장 작은 3개의 숫자 찾기
-            std::vector<int> smallestThree(numbers);
-            std::sort(smallestThree.begin(), smallestThree.end());
+        minHeap.push(num);
 
-            int product = smallestThree[0] * smallestThree[1] * smallestThree[2];
-            std::cout << product << std::endl;
+        // 현재까지의 숫자가 3개보다 많으면 가장 작은 수를 제거
+        if (minHeap.size() > 3) {
+            minHeap.pop();
+        }
+
+        // 아직 3개의 숫자가 채워지지 않았으면 -1 출력
+        if (minHeap.size() < 3) {
+            cout << "-1" << endl;
         } else {
-            // 주어진 숫자의 수가 3개 미만인 경우
-            std::cout << -1 << std::endl;
+            // 가장 작은 3개의 숫자의 곱 출력
+            long long product = 1;
+            priority_queue<int, vector<int>, greater<int>> tempHeap = minHeap;
+            while (!tempHeap.empty()) {
+                product *= tempHeap.top();
+                tempHeap.pop();
+            }
+            cout << product << endl;
         }
     }
 
