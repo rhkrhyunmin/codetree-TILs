5bc41a8617253bb9fa65980e5c22525c3fdfac54
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-18 10:17:09 +0900

    Title: 10개 입력받기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240118/10개 입력받기/receive-10-inputs.cpp
+++ 240118/10개 입력받기/receive-10-inputs.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
-using namespace std;
+#include <iomanip>  // setprecision을 사용하기 위한 헤더 파일
 
 int main() {
+    int numbers[10];
+    int sum = 0;
+    int count = 0;
 
-    int arr[10];
+    // 10개의 정수 입력 받기
     for (int i = 0; i < 10; ++i) {
-        cin >> arr[i];
+        std::cin >> numbers[i];
+
+        // 0이 입력되면 입력 중단
+        if (numbers[i] == 0) {
+            break;
+        }
+
+        // 합과 개수 업데이트
+        sum += numbers[i];
+        count++;
     }
 
-    double sum = 0;
-    for (int i = 0; i < 10; ++i) {
-        sum += arr[i];
-    }
+    // 0을 제외한 합과 평균 계산
+    double average = (count > 0) ? static_cast<double>(sum) / count : 0.0;
 
-    double avarage = 0;
-    for (int i = 0; i < 10; i++)
-    {
-        avarage = sum / 10.0;
-    }
-
-    cout << sum << endl;
-    cout << avarage << endl;
+    // 결과 출력
+    std::cout <<  sum << " " << std::fixed << std::setprecision(1) << average << std::endl;
 
     return 0;
 }
