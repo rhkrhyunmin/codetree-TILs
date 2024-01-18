c79683653f536998a2c535230b1510a3f32c771d
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-18 17:32:18 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240118/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.cpp
+++ 240118/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
-using namespace std;
 
-int main()
-{
-	int n[10];
+int main() {
+    const int size = 10;
+    int numbers[size];
 
-	int count = 0;
-	int result = 0;
-	for (int i = 0; i < 10; i++)
-	{
-		cin >> n[i];
-		while (n[i] != 0)
-		{
-			if (n[i] % 2 == 0)
-			{
-				count++;
-				result += n[i];
-			}
-			break;
-		}
-	}
-	cout << count << " " << result;
+    // 10개의 정수 입력 받기
+    for (int i = 0; i < size; ++i) {
+        std::cin >> numbers[i];
+    }
+
+    // 2의 배수의 개수와 합계 계산
+    int countOfMultiplesOfTwo = 0;
+    int sumOfMultiplesOfTwo = 0;
+
+    for (int i = 0; i < size; ++i) {
+        if (numbers[i] == 0) {
+            break; 
+        }
+
+        if (numbers[i] % 2 == 0) {
+            countOfMultiplesOfTwo++;
+            sumOfMultiplesOfTwo += numbers[i];
+        }
+    }
+
+    // 결과 출력
+    std::cout << countOfMultiplesOfTwo << " " << sumOfMultiplesOfTwo << std::endl;
+
+    return 0;
 }
