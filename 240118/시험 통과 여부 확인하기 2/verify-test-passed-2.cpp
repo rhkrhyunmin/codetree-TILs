688bc8a3533a0c9701a80528209a8812bf162da4
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-18 17:42:24 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240118/시험 통과 여부 확인하기 2/verify-test-passed-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int count = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		int score[4];
+
+		for (int j = 0; j < 4; j++)
+		{
+			cin >> score[j];
+		}
+
+		int totalScore = 0;
+		for (int j = 0; j < 4; j++)
+		{
+			totalScore += score[j];
+		}
+
+		int avarage = totalScore / 4;
+
+		if (avarage >= 60)
+		{
+			cout << "Pass" << endl;
+			count++;
+		}
+		else {
+			cout << "fail" << endl;
+		}
+	}
+
+	cout << count << endl;
+}
