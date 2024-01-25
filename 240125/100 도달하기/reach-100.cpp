09f1ca64e9974880d1d23c9564d91ef853c9bbf5
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-25 14:03:58 +0900

    Title: 100 도달하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240125/100 도달하기/reach-100.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main() {
+    int n;
+
+    std::cin >> n;
+
+    if (n < 1 || n > 10) {
+        return 1; // 프로그램 종료
+    }
+
+    int firstTerm = 1;
+    int secondTerm = n;
+
+    // 첫 번째 항 출력
+    std::cout << firstTerm << " ";
+
+    // 두 번째 항 출력
+    std::cout << secondTerm << " ";
+
+    // 세 번째 항부터 계산하여 출력
+    while (true) {
+        int nextTerm = firstTerm + secondTerm;
+        if (nextTerm > 200) {
+            break; // 100을 넘으면 종료
+        }
+
+        std::cout << nextTerm << " ";
+
+        // 다음 항을 위해 갱신
+        firstTerm = secondTerm;
+        secondTerm = nextTerm;
+    }
+
+    return 0;
+}
