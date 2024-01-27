274c69643b0bb2b8b5e80ba4e64350742d0712b5
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-27 22:49:25 +0900

    Title: 코로나 메뉴얼2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240127/코로나 메뉴얼2/covid-manual2.cpp
+++ 240127/코로나 메뉴얼2/covid-manual2.cpp
@@ -1,51 +1,56 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
-    int countA = 0, countB = 0, countC = 0, countD = 0, countE = 0;
-    char ch;
+    char symptom1, symptom2, symptom3;
+    double temperature1, temperature2, temperature3;
 
-    // 3명의 감기 증상과 체온 입력
-    for (int i = 0; i < 3; ++i) {
-        char symptom;
-        double temperature;
+    // 환자 1 정보 입력
+    std::cin >> symptom1 >> temperature1;
+    // 환자 2 정보 입력
+    std::cin >> symptom2 >> temperature2;
+    // 환자 3 정보 입력
+    std::cin >> symptom3 >> temperature3;
 
-        cin >> symptom >> temperature;
+    // 진료소 인원 초기화
+    int countA = 0, countB = 0, countC = 0, countD = 0;
 
-        if (symptom == 'Y' && temperature >= 37.0) {
-            // A: 증상 있고 체온 37℃ 이상
-            countA++;
-        }
-        else if (symptom == 'N' && temperature >= 37.0) {
-            // B: 증상 없고 체온 37℃ 이상
-            countB++;
-        }
-        else if (symptom == 'Y' && temperature < 37.0) {
-            // C: 증상 있고 체온 정상
-            countC++;
-        }
-        else if (symptom == 'N' && temperature < 37.0) {
-            // D: 둘 다 괜찮은 경우
-            countD++;
-        }
+    // A, B, C, D 진료소 인원 계산
+    if (symptom1 == 'Y' && temperature1 >= 37) {
+        countA++;
+    } else if (symptom1 == 'N' && temperature1 >= 37) {
+        countB++;
+    } else if (symptom1 == 'Y' && temperature1 < 37) {
+        countC++;
+    } else {
+        countD++;
     }
 
-    // E: 위급상황인 경우 계산
+    if (symptom2 == 'Y' && temperature2 >= 37) {
+        countA++;
+    } else if (symptom2 == 'N' && temperature2 >= 37) {
+        countB++;
+    } else if (symptom2 == 'Y' && temperature2 < 37) {
+        countC++;
+    } else {
+        countD++;
+    }
+
+    if (symptom3 == 'Y' && temperature3 >= 37) {
+        countA++;
+    } else if (symptom3 == 'N' && temperature3 >= 37) {
+        countB++;
+    } else if (symptom3 == 'Y' && temperature3 < 37) {
+        countC++;
+    } else {
+        countD++;
+    }
+
+    // 위급상황 여부 확인 및 출력
     if (countA >= 2) {
-        countE = 1;
+        std::cout << countA << " " << countB << " " << countC << " " << countD << " E" << std::endl;
+    } else {
+        std::cout << countA << " " << countB << " " << countC << " " << countD << std::endl;
     }
-
-    if (countB > 1)
-    {
-        cout << countA << " " << countB << " " << countC << " " << countD << " " << "E";
-    }
-    else {
-        // 각 진료소에 보내지는 인원 출력
-        cout << countA << " " << countB << " " << countC << " " << countD << " ";
-    }
-
-    
 
     return 0;
 }
