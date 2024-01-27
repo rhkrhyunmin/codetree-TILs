3ca41a5790c63009898891cc1342d71eb7ffc337
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-27 14:41:52 +0900

    Title: 점수대 파악하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240127/점수대 파악하기/find-out-the-score-range.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> scoreCounts(11, 0); // 10점 단위로 구분된 점수대별 학생 수를 저장하는 배열
+
+    while (true) {
+        int score;
+        cin >> score;
+
+        if (score == 0) {
+            break; // 0이 입력되면 입력 종료
+        }
+
+        if (score >= 10) {
+            int scoreIndex = score / 10; // 10점 단위로 구분된 인덱스 계산
+            scoreCounts[scoreIndex]++; // 해당 점수대별 학생 수 증가
+        }
+    }
+
+    // 각 점수대별 학생 수 출력
+    for (int i = 1; i < 11; ++i) {
+        cout << i * 10 << " - " << scoreCounts[i] << endl;
+    }
+
+    return 0;
+}
