90bbef97702e38ad30bdd48c6bd69aa0d32c689f
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-27 14:52:56 +0900

    Title: 코로나 메뉴얼2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240127/코로나 메뉴얼2/covid-manual2.cpp
+++ 240127/코로나 메뉴얼2/covid-manual2.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    int countA = 0, countB = 0, countC = 0, countD = 0, countE = 0;
+    int countA = 0, countB = 0, countC = 0;
 
     // 3명의 감기 증상과 체온 입력
     for (int i = 0; i < 3; ++i) {
@@ -21,19 +21,15 @@ int main() {
         } else if (symptom == 'Y' && temperature < 37.0) {
             // C: 증상 있고 체온 정상
             countC++;
-        } else if (symptom == 'N' && temperature < 37.0) {
-            // D: 둘 다 괜찮은 경우
-            countD++;
         }
     }
 
     // E: 위급상황인 경우 계산
     if (countA >= 2) {
-        countE = 1;
+        cout << countA << " " << countB << " " << countC << " E";
+    } else {
+        cout << countA << " " << countB << " " << countC;
     }
-
-    // 각 진료소에 보내지는 인원 출력
-    cout << countA << " " << countB << " " << countC << " " << countD << " " << countE;
 
     return 0;
 }
