c20e94b92105883524a212506ac88fcb32c4c403
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-01-28 14:16:11 +0900

    Title: 배열의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240128/배열의 합/sum-of-array.cpp
+++ 240128/배열의 합/sum-of-array.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
-using namespace std;
 
-int main()
-{
-	int arr[4][4];
+int main() {
+    // 4x4 크기의 배열을 선언합니다.
+    int arr[4][4];
 
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			cin >> arr[i][j];
-		}
-	}
+    // 배열에 각 원소를 입력받습니다.
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            std::cin >> arr[i][j];
+        }
+    }
 
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			cout << arr[i][j] << " ";
-		}
-		cout << endl;
-	}
+    // 각 줄의 합을 계산하고 출력합니다.
+    for (int i = 0; i < 4; ++i) {
+        int lineSum = 0;  // 현재 줄의 합을 저장하는 변수
+        for (int j = 0; j < 4; ++j) {
+            lineSum += arr[i][j];
+        }
+        std::cout << lineSum << std::endl;
+    }
+
+    return 0;
 }
