175cbd0b7e4c95c0daf1f1707583dc9e7f0c8c32
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-02-03 12:18:57 +0900

    Title: 문자열 범위 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240203/문자열 범위 출력하기/print-string-in-range.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string str;
+
+	getline(cin, str);
+
+	cout << str[2] << str[3] << str[4] << str[5] << str[6] << str[7] << str[8] << str[9];
+	
+	return 0;
+
+}
