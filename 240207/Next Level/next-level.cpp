8f5ed5eb008629b8e7172bc3fd73bcd930a648b2
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-02-07 12:09:11 +0900

    Title: Next Level, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240207/Next Level/next-level.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class User {
+public:
+    string id;
+    int level;
+
+    // 생성자 정의
+    User(string id, int level) {
+        this->id = id;
+        this->level = level;
+    }
+
+    // 사용자 정보 출력 메서드
+    void printUserInfo() {
+        cout << "user " << id << " lv " << level << endl;
+    }
+};
+
+int main() {
+    string id;
+    int level;
+
+    // 첫 번째 입력
+    cin >> id >> level;
+
+    // 첫 번째 사용자 출력
+    User user1("codetree", 10);
+    user1.printUserInfo();
+
+    // 두 번째 입력
+    cin >> id >> level;
+
+    // 두 번째 사용자 출력
+    User user2(id, level);
+    user2.printUserInfo();
+
+    return 0;
+}
