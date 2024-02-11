9859cbbe6cc7e40fc995086392f9833eae892823
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-02-11 20:03:37 +0900

    Title: 사는 지역, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240211/사는 지역/where-live.cpp
+++ 240211/사는 지역/where-live.cpp
@@ -2,36 +2,41 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
-// 사람 정보를 담는 구조체
-struct Person {
+class Person {
+public:
     string name;
     string address;
-    string streetNumber;
-};
+    string id;
 
-// 사전 순으로 이름을 비교하기 위한 비교 함수
-bool compareNames(const Person& p1, const Person& p2) {
-    return p1.name < p2.name;
-}
+    Person(string n, string a, string i) : name(n), address(a), id(i) {}
+
+    // 비교 연산자 오버로딩
+    bool operator<(const Person& other) const {
+        // 이름을 기준으로 비교
+        return name < other.name;
+    }
+};
 
 int main() {
     int n;
     cin >> n;
+    
+    vector<Person> people;
 
-    vector<Person> people(n);
-
-    // 사람들의 정보 입력
     for (int i = 0; i < n; ++i) {
-        cin >> people[i].name >> people[i].streetNumber >> people[i].address;
+        string name, address, id;
+        cin >> name >> id >> address;
+        people.push_back(Person(name, address, id));
     }
 
-    // 이름을 기준으로 정렬
-    sort(people.begin(), people.end(), compareNames);
+    // 사람들을 이름 기준으로 정렬
+    sort(people.begin(), people.end());
 
-    // 가장 느린 사람의 정보 출력
-    cout << "name" << people[n - 1].name << " " << endl << "addr" <<people[n - 1].streetNumber<< " " << endl << "city" << people[n - 1].address << endl;
+    // 이름이 가장 느린 사람 출력
+    cout << people.back().name << " " << people.back().id << " " << people.back().address << endl;
 
     return 0;
 }
