2099d563515f9d85e50c2bc58864561892c75d2a
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-02-14 14:22:04 +0900

    Title: 10진수로 변환하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240214/10진수로 변환하기/convert-to-decimal.cpp
+++ 240214/10진수로 변환하기/convert-to-decimal.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <string>
+#include <cmath>
+
 using namespace std;
 
 int main() {
-    int binary[5] = { 1, 1, 1, 0, 1 };
-    int num = 0;
+    string binary;
+    cin >> binary;
 
-    for (int i = 0; i < 5; i++)
-        num = num * 2 + binary[i];
+    int decimal = 0;
+    int power = 0;
 
-    cout << num;
+    // 입력된 2진수를 뒤에서부터 시작하여 각 자리에 해당하는 10진수 값을 더합니다.
+    for (int i = binary.size() - 1; i >= 0; --i) {
+        if (binary[i] == '1') {
+            decimal += pow(2, power);
+        }
+        power++;
+    }
+
+    cout << decimal << endl;
+
+    return 0;
 }
